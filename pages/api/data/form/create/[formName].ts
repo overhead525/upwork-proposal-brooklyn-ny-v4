9b8e188d3fa06149b9f9b6f05d88fc16d47708f1
overhead525 <ritchelcousar@outlook.com>
2021-02-13 09:06:24 -0500

    created a dummy route demonstrating successful api call that uses database mongodb

---
+++ pages/api/data/form/create/[formName].ts
@@ -0,0 +1,30 @@
+import { FormElement, FormObject } from "../../../../../models/interfaces";
+import { Form } from "../../../../../models";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { db } from "../../../../../lib/database/mongo-client";
+
+import { FormDoc } from "../../../../../models/types";
+
+const nullFormElement: FormElement = {
+  question: null,
+  type: null,
+  questionKey: null,
+  helperText: null,
+  choices: [null],
+};
+
+const nullFormObject: FormObject = {
+  title: null,
+  pages: [[nullFormElement]],
+  url: null,
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body);
+
+  const numUsers = await db.collection("users").countDocuments();
+  console.log(`There are ${numUsers} users in the "users" collection`);
+
+  res.send("performed request");
+};
