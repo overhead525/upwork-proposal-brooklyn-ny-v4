b94403cf92883515502786cdd606745b3c8a8ab5
overhead525 <ritchelcousar@outlook.com>
2021-02-14 13:32:17 -0500

    added a file full of constants we can use to write tests more efficiently and quickly

---
+++ test/constants/form.ts
@@ -0,0 +1,81 @@
+// A file to store constants for tests in so we can re-use them
+
+import {
+  formElementType,
+  FormElement,
+  FormObject,
+  Form,
+} from "../../models/interfaces";
+import { createScrambledArray } from "../../helpers";
+
+export const sampleFormElements: FormElement[] = [
+  {
+    question: "How old are you?",
+    type: formElementType.SHORT_ANSWER,
+    questionKey: "howOldAreYou",
+    helperText: "Enter a number between 1 and 100 (endpoints inclusive)",
+  },
+  {
+    question: "What would you like to eat?",
+    type: formElementType.CHECKBOXES,
+    questionKey: "whatWouldYouLikeToEat",
+    helperText: "Choose as many as you'd like",
+    choices: [
+      "McDouble",
+      "McFlurry",
+      "Chicken Fries",
+      "Onion Rings",
+      "Boneless Honey BBQ Wings",
+      "Blueberry Smoothie",
+      "Grilled Tilapia",
+    ],
+  },
+  {
+    question: "How would you like to pay?",
+    type: formElementType.RADIO,
+    questionKey: "howWouldYouLikeToPay",
+    choices: ["Debit Card", "Paypal", "Venmo", "Stripe", "Google Pay"],
+  },
+];
+
+const firstFormElementScramble: FormElement[] = createScrambledArray(
+  sampleFormElements
+);
+
+const secondFormElementScramble: FormElement[] = createScrambledArray(
+  sampleFormElements
+);
+
+export const sampleFormObjectV1Preview: FormObject = {
+  title: "Taking the Order v1 (preview)",
+  pages: [firstFormElementScramble, secondFormElementScramble],
+  url: "https://amazon.co.us/east-2/form/s9dkl2k/taking-the-order-v1/preview",
+};
+
+export const sampleFormObjectV1: FormObject = {
+  title: "Taking the Order v1",
+  pages: [firstFormElementScramble, secondFormElementScramble],
+  url: "https://amazon.co.us/east-2/form/s9dkl2k/taking-the-order-v1",
+};
+
+export const sampleFormObjectV2Preview: FormObject = {
+  title: "Taking the Order v2",
+  pages: [secondFormElementScramble, firstFormElementScramble],
+  url: "https://amazon.co.us/east-2/form/fj2ls92/taking-the-order-v2/preview",
+};
+
+export const sampleFormObjectV2: FormObject = {
+  title: "Taking the Order v2",
+  pages: [secondFormElementScramble, firstFormElementScramble],
+  url: "https://amazon.co.us/east-2/form/fj2ls92/taking-the-order-v2",
+};
+
+export const sampleFormV1: Form = {
+  preview: sampleFormObjectV1Preview,
+  published: sampleFormObjectV1,
+};
+
+export const sampleFormV2: Form = {
+  preview: sampleFormObjectV2Preview,
+  published: sampleFormObjectV2,
+};
