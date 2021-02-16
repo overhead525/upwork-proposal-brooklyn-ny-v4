20b9a495077a08ab0e5c43d88d139f94898696ca
overhead525 <ritchelcousar@outlook.com>
2021-02-16 10:11:38 -0500

    added tests for the formObject schema

--- test/formModel/formObject.test.ts
+++ test/formModel/formObject.test.ts
@@ -4,7 +4,7 @@ import { sampleFormObjectV1 } from "../constants/form";
 import { runValidationTestLogic } from "./sharedLogic";
 
 describe("formObject schema validation is tight", () => {
-  const missingPropertyValidationTest = (missingProp: string) => {
+  const missingPropertyValidationTest = async (missingProp: string) => {
     const testFormObjectDoc: FormObjectDoc = { ...sampleFormObjectV1 };
     Object.defineProperty(testFormObjectDoc, missingProp, {
       value: null,
@@ -12,10 +12,16 @@ describe("formObject schema validation is tight", () => {
 
     const testFormObject = new FormObject(testFormObjectDoc);
 
-    runValidationTestLogic(missingProp, testFormObject);
+    await runValidationTestLogic(missingProp, testFormObject);
   };
 
-  test("fails if no title property is provided", async () => {
-    missingPropertyValidationTest("title");
+  test("fails if no 'title' property is provided", async () => {
+    await missingPropertyValidationTest("title");
+  });
+  test("fails if no 'pages' property is provided", async () => {
+    await missingPropertyValidationTest("pages");
+  });
+  test("fails if no 'url' property is provided", async () => {
+    await missingPropertyValidationTest("url");
   });
 });
