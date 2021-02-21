af49ea28df9f0621c9f3f5d601fd4bab57a135eb
overhead525 <ritchelcousar@outlook.com>
2021-02-21 17:31:24 -0500

    created new environment files for test-database and production-database, without commiting database password to git repository

--- lib/database/mongo-client.ts
+++ lib/database/mongo-client.ts
@@ -1,8 +1,20 @@
 import * as mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const filledURL = process.env.MONGO_URL.replace(
+  "<variable>",
+  process.env.DATABASE_ROOT_PASSWORD
+);
+
+mongoose.connect(
+  filledURL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () =>
+    console.log(
+      process.env.DATABASE_ROOT_PASSWORD + "\n" + process.env.MONGO_URL
+    )
+);
 
 export const db = mongoose.connection;
