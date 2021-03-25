007589a65c6b1d04e247f1036f73b7ca648df2f5
overhead525 <ritchelcousar@outlook.com>
2021-03-25 15:16:14 -0400

    implemented dataSource methods for form

--- lib/apollo/data-sources/Forms.ts
+++ lib/apollo/data-sources/Forms.ts
@@ -10,11 +10,61 @@ import {
 } from "../../../src/generated/graphql";
 
 export class Forms extends MongoDataSource<Form> {
-  async createForm(args: MutationCreateFormArgs): Maybe<Form> {}
+  async createForm(args: MutationCreateFormArgs): Promise<Maybe<Form>> {
+    const newForm: Form = {
+      preview: {
+        title: args.previewTitle,
+        pages: args.previewPages,
+      },
+      published: {
+        title: args.publishedTitle,
+        pages: args.publishedPages,
+      },
+    };
 
-  async getForm(args: QueryGetFormArgs): Maybe<Form> {}
+    try {
+      const response = await this.collection.insertOne(newForm);
+      return response.ops[0];
+    } catch (error) {
+      return error;
+    }
+  }
 
-  async updateForm(args: MutationUpdateFormArgs): Maybe<Form> {}
+  async getForm(args: QueryGetFormArgs): Promise<Maybe<Form>> {
+    try {
+      const response = await this.findOneById(args.formID);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
 
-  async deleteForm(args: MutationDeleteFormArgs): Scalars["Boolean"] {}
+  async updateForm(args: MutationUpdateFormArgs): Promise<Maybe<Form>> {
+    try {
+      const form = await this.findOneById(args.formID);
+      const { formID, ...changes } = args;
+      const newForm = {
+        ...form,
+        ...changes,
+      };
+      const response = await this.collection.findOneAndUpdate(
+        { id: formID },
+        newForm
+      );
+      return response.value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteForm(args: MutationDeleteFormArgs): Promise<Scalars["Boolean"]> {
+    try {
+      const response = await this.collection.findOneAndDelete({
+        id: args.formID,
+      });
+      return response.ok === 1;
+    } catch (error) {
+      return error;
+    }
+  }
 }
