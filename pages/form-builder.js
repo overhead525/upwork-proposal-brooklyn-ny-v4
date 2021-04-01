748de0813adcfe946362ba1fff9d80ed183126fd
overhead525 <ritchelcousar@outlook.com>
2021-04-01 12:51:13 -0400

    using redux for state management

--- pages/form-builder.js
+++ pages/form-builder.js
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
 import { useSession } from "next-auth/client";
 import { FormBuilder } from "../components/formBuilder";
+import store from "../store";
 
 export default function App() {
   const [session, loading] = useSession();
 
-  return <FormBuilder username="marcus" />;
+  return (
+    <Provider store={store}>
+      <FormBuilder username="marcus" />
+    </Provider>
+  );
 }
