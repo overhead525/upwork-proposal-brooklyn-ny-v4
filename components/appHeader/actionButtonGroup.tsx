b0c11d2f39f162067dd14f0fb52d3e86769e90be
overhead525 <ritchelcousar@outlook.com>
2021-05-17 19:25:24 -0400

    made view button follow a link to the active published form

--- components/appHeader/actionButtonGroup.tsx
+++ components/appHeader/actionButtonGroup.tsx
@@ -1,4 +1,11 @@
 import Button, { ButtonGroup } from "@atlaskit/button";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  currentFormIndexSelector,
+  formDataSelector,
+} from "../../features/userData/userDataSlice";
 
 export interface ActionButtonProps {
   text: string | JSX.Element;
@@ -19,17 +26,46 @@ interface ActionButtonGroupProps {
 export const ActionButtonGroup: React.FC<ActionButtonGroupProps> = ({
   buttons,
 }) => {
+  const formData = useSelector(formDataSelector);
+  const currentFormIndex = useSelector(currentFormIndexSelector);
+  const currentFormID = Object.keys(formData)[currentFormIndex];
+  const [current, setCurrent] = useState(null);
+
+  useEffect(() => {
+    if (currentFormID) {
+      setCurrent(currentFormID);
+    }
+  }, [currentFormID]);
+
+  const ButtonWrapper = (button: ActionButtonProps): JSX.Element => {
+    const outputtedButton = (
+      <Button
+        appearance={button.appearance}
+        key={button.title}
+        style={{ marginRight: "0.65rem" }}
+      >
+        {button.text}
+      </Button>
+    );
+
+    return currentFormID && button.title.toLowerCase() === "view" ? (
+      <a
+        href={`/forms/${currentFormID}`}
+        target="_blank"
+        style={{ textDecoration: "none" }}
+      >
+        {outputtedButton}
+      </a>
+    ) : (
+      outputtedButton
+    );
+  };
+
   return (
     <ButtonGroup>
       {buttons.map((button, index, arr) => {
         return index < arr.length - 1 ? (
-          <Button
-            appearance={button.appearance}
-            key={button.title}
-            style={{ marginRight: "0.65rem" }}
-          >
-            {button.text}
-          </Button>
+          ButtonWrapper(button)
         ) : (
           <Button appearance={button.appearance} key={button.title}>
             {button.text}
